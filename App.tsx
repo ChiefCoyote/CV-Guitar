f3e0279c9bb5db96fff061e4ae40aa23b059c7e2
Oliver Cordeaux <126358259+ChiefCoyote@users.noreply.github.com>
2024-10-22 00:12:47 +0100

    Created project, Display front camera feed on the screen

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import {Camera, useCameraDevice, useCameraPermission} from 'react-native-vision-camera';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -62,37 +63,23 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const device = useCameraDevice('front');
+  const {hasPermission, requestPermission} = useCameraPermission();
+
+  useEffect(() => {
+      requestPermission();
+  }, [requestPermission]);
+
+  if (!hasPermission) {
+      return <Text>No permission</Text>;
+  }
+
+  if (device == null) {
+        return <Text>No device</Text>;
+  }
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
   );
 }
 
